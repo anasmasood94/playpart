0a4fc20920d62bb4d932b3502a6faa99bb60c92c
Anas Masood <anasmasood94@gmail.com>
2021-10-04 15:32:07 +0500

    Add description to videos

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -1,46 +1,46 @@
 class Api::V1::VideosController < Api::V1::ApiController
-    
-    def create
-        video = current_user.videos.new(create_params)
-        if video.save
-            video_duration video   
-        else
-            render json: video.errors, status: 403
-        end
+
+  def create
+    video = current_user.videos.new(create_params)
+    if video.save
+      video_duration video
+    else
+      render json: video.errors, status: 403
     end
+  end
 
-    def destroy
-        video = current_user.videos.find_by_id(params[:id])
-        if video.present?
-          video.destroy
-          render json: { success: true, msg: 'video Deleted Successfully.' }, status: 200
-        else
-          render json: { success: false, message: "Can't find video" }, status: 422
-        end
+  def destroy
+    video = current_user.videos.find_by_id(params[:id])
+    if video.present?
+      video.destroy
+      render json: { success: true, msg: 'video Deleted Successfully.' }, status: 200
+    else
+      render json: { success: false, message: "Can't find video" }, status: 422
     end
+  end
 
-    def get_video_list
+  def get_video_list
+    videos = Video.paginate(page: params[:page] || 1, per_page: params[:per_page] || 20)
 
-        video = Video.paginate(page: params[:page] || 1, per_page: params[:per_page] || 20)
-        if video.present?
-            render json: video.order(updated_at: :desc), each_serializer: ::VideoSerializer::Base, adapter: :json
-        else
-            render json: { success: false, message: "Can't find video" }, status: 422
-        end
-    end   
-
-    private
-    def create_params
-        params.permit(:video, :name)
+    if videos.present?
+      render json: videos.order(updated_at: :desc), each_serializer: ::VideoSerializer::Base, adapter: :json
+    else
+      render json: { success: false, message: "Can't find video" }, status: 422
     end
+  end
 
-    def video_duration video      
-        duration = ActiveStorage::Analyzer::VideoAnalyzer.new(video.video.blob).metadata[:duration]
-        if duration > 15
-            video.destroy
-            render json: { success: false, message: "Can't upload video of duration greater than 15 sec" }, status: 422
-        else
-            render json: { success: true, id: video.id }.to_json, status: 200
-        end
+  private
+  def create_params
+    params.permit(:video, :name, :description)
+  end
+
+  def video_duration video
+    duration = ActiveStorage::Analyzer::VideoAnalyzer.new(video.video.blob).metadata[:duration]
+    if duration > 15
+      video.destroy
+      render json: { success: false, message: "Can't upload video of duration greater than 15 sec" }, status: 422
+    else
+      render json: { success: true, id: video.id }.to_json, status: 200
     end
+  end
 end
--- app/serializers/video_serializer/base.rb
+++ app/serializers/video_serializer/base.rb
@@ -1,12 +1,12 @@
 module VideoSerializer
   class Base < ActiveModel::Serializer
-    attributes  :id, :name, :created_at, :updated_at, :result_video_url
-  
+    attributes  :id, :name, :created_at, :updated_at, :result_video_url, :description
+
     def result_video_url
       if object.video.attached?
         object.video.service_url
       end
-    end 
+    end
   end
 end
-  
+
