5def5061be6b00a01a57a567067ac327e7265a39
Anas Masood <anasmasood94@gmail.com>
2021-11-03 16:13:38 +0500

    Update the migration

--- db/migrate/20211029151401_unique_user_name.rb
+++ db/migrate/20211029151401_unique_user_name.rb
@@ -1,7 +1,7 @@
 class UniqueUserName < ActiveRecord::Migration[6.1]
   def up
     add_column :users, :username, :string
-    execute "UPDATE users SET username = CONCAT('user', floor(random()* (400-100 + 1) + 100)::varchar(255)) WHERE username IS NULL"
+    execute "UPDATE users SET username = CONCAT('user', floor(random()* (100000-100 + 1) + 100)::varchar(255)) WHERE username IS NULL"
     add_index :users, :username, unique: true
   end
 
